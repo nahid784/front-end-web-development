70ff09ee5979e371bcb1ebdd10624ea54c0f71ff
Nahid Hasan Chowdhury <112480805+nahid784@users.noreply.github.com>
2025-01-19 23:51:03 +0600

    Update script.js

--- JS/ch1_introduction/script.js
+++ JS/ch1_introduction/script.js
@@ -1,25 +1,22 @@
 alert("Hello this is an alert msg");
 
-console.log("Hey, it is a console")
-console.log("Code is running")
-console.log("we can see execution steps")
+console.log("Hey, it is a console");
+console.log("Code is running");
+console.log("we can see execution steps");
 
-var a = prompt("Enter your name")
-var isTrue = confirm("Are you sure you want to submit this form ")
-if(isTrue){
-    console.log("Form Submitted")
+var a = prompt("Enter your name");
+var isTrue = confirm("Are you sure you want to submit this form ");
+if (isTrue) {
+  console.log("Form Submitted");
+} else {
+  console.log("Form not submitted");
 }
+console.log("youe name is " + a);
 
-else{
-    console.log("Form not submitted")
-}
-console.log("youe name is " +a)
+document.title = "It is Js tutorial phase";
 
-document.title = "It is Js tutorial phase"
-
-document.body.style.backgroundColor= "red"
+document.body.style.backgroundColor = "red";
 
 // silly code to demonstrate output
 document.write("Hello world!");
-document.write("<p>How are <br/>" +
-"<i>you</i>?</p>");
+document.write("<p>How are <br/>" + "<i>you</i>?</p>");
