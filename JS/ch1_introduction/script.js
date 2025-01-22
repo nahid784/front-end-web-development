5f719514c0b22b2c0a4886ffffeab5411a01efce
nahid784 <nahidhasantusher784@gmail.com>
2025-01-23 02:17:29 +0600

    Update script.js

--- JS/ch1_introduction/script.js
+++ JS/ch1_introduction/script.js
@@ -1,1 +1,15 @@
 alert("Hello World");   // it will show alert box
+
+console.log("Hey It is a console..") // will show the text in console developer option
+
+var a = prompt("Enter your number:");
+var istrue = confirm("this is a confirmation prompt") // confirmation popup start
+if(istrue){
+  console.log("confirmation is true")
+}
+else{
+  console.log("confirmation is false")
+}
+// confirmation popup end
+
+console.log("your number is " + a);
