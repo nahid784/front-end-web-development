5f025ff208bae19f1831fdbc8994a01bb925709c
nahid784 <nahidhasantusher784@gmail.com>
2025-01-24 20:53:33 +0600

    Update script.js

--- JS/ch2_js-variables/script.js
+++ JS/ch2_js-variables/script.js
@@ -1,6 +1,15 @@
-var a = 50;
-var b = 60;
-var c = "Nahid";    // js automatically detects the var datatypes.
+// var a = 50;
+// var b = 60;
+// var c = "Nahid";    // js automatically detects the var datatypes.
+
+
+let a = 50; 
+// use let instead of var in modern js
+let b = 60;
+
+let c = "Nahid";
+
+
 
 console.log(a + b + 8)
 
@@ -11,3 +20,8 @@ console.log(typeof a, typeof b, typeof c)
 const a1 = 6;
 
 a1 = a1 + 1;  // not allowed as a1 is declared as constant 
+
+{
+    let a = 40; // as let is a block scope
+    console.log(a);
+}
