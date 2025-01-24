9e9d537433ebfeda798bfcf0aa9caeabc808c6eb
nahid784 <nahidhasantusher784@gmail.com>
2025-01-24 20:48:10 +0600

    Update script.js

--- JS/ch2_js-variables/script.js
+++ JS/ch2_js-variables/script.js
@@ -1,4 +1,13 @@
 var a = 50;
 var b = 60;
-var c = "Nahid";
-// js automatically detects the var datatypes.
+var c = "Nahid";    // js automatically detects the var datatypes.
+
+console.log(a + b + 8)
+
+console.log(typeof a, typeof b, typeof c)
+
+// it will print the type of variable datatypes
+
+const a1 = 6;
+
+a1 = a1 + 1;  // not allowed as a1 is declared as constant 
