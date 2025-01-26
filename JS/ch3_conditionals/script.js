bcb18547c60d167d4d26ce5001ce8e2e482d3f68
nahid784 <nahidhasantusher784@gmail.com>
2025-01-27 00:49:47 +0600

    Update script.js

--- JS/ch3_conditionals/script.js
+++ JS/ch3_conditionals/script.js
@@ -1,11 +1,20 @@
 console.log("It is a conditional if else of js")
 
-let age = 45;
+let age = 17;
+let grace = 2;
 
+// with grace
+if(age + grace > 18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cant drive")
+}
+
+// without grace
 if(age > 18){
     console.log("you can drive");
 }
-
 else{
     console.log("you cant drive")
 }
