38fa6eabfcc2b0d3974c85c6016e4cc59f9ab0f7
nahid784 <nahidhasantusher784@gmail.com>
2025-01-28 23:34:59 +0600

    Update script.js

--- JS/ch3_conditionals/script.js
+++ JS/ch3_conditionals/script.js
@@ -3,18 +3,39 @@ console.log("It is a conditional if else of js")
 let age = 17;
 let grace = 2;
 
+// Arithmetic Operators
+console.log("Summation is ", age + grace);
+console.log("Substract is ", age - grace);
+console.log("Multiplication is ", age * grace);
+console.log("Division is ", age / grace);
+console.log("Power of ", age, "^", grace, "is", age ** grace);
+console.log("Remainder or Modulus is ", age % grace) ;
+
+// Assignment Operators
+age += grace;   // age = age + grace;
+console.log("Updated Age is ", age);
+
+age -= grace;   // age = age - grace;
+console.log("Updated Age is ", age);
+
+
 // with grace
-if(age + grace > 18){
+if (age + grace > 18) {
     console.log("you can drive");
 }
-else{
+else {
     console.log("you cant drive")
 }
 
 // without grace
-if(age > 18){
+if (age > 18) {
     console.log("you can drive");
 }
-else{
+else {
     console.log("you cant drive")
+}
+
+// equal to
+if (age == 18) {
+    console.log("you can drive");
 }
