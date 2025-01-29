ea97fd27ef1911ae770ec809c7841b6fb3a3f598
nahid784 <nahidhasantusher784@gmail.com>
2025-01-30 01:08:59 +0600

    loops

---
+++ JS/ch4_loops/loops.js
@@ -0,0 +1,15 @@
+console.log("Loops Tutorial");
+
+let a = 1;
+
+// before loop
+console.log("Before Loop");
+console.log(a);
+console.log(a + 1);
+console.log(a + 2);
+
+// for loop
+for (let i = 0; i < 100; i++) {
+    console.log(a+i);
+    
+}
