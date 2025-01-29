be6798d6ada5e1b89955333bdc0f91dbae655808
nahid784 <nahidhasantusher784@gmail.com>
2025-01-30 01:16:33 +0600

    Update loops.js

--- JS/ch4_loops/loops.js
+++ JS/ch4_loops/loops.js
@@ -13,3 +13,26 @@ for (let i = 0; i < 100; i++) {
     console.log(a+i);
     
 }
+
+// for in loop
+// it is used for object
+let obj = {
+    name: "Nahid",
+    role: "Developer",
+    company: "Google"
+}
+
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key, element)
+        console.log(element)
+        
+    }
+}
+
+// for-off loop
+// its used for array and string
+for (const char of "Nahid") {
+    console.log(char);  
+} 
