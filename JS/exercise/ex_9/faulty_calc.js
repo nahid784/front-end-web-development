8c09029df4549c640c258663c6407fe863e968fc
nahid784 <nahidhasantusher784@gmail.com>
2025-01-30 19:43:41 +0600

    faulty calculator

---
+++ JS/exercise/ex_9/faulty_calc.js
@@ -0,0 +1,11 @@
+/* create a faulty calculator using js
+it does the following:
+1. It takes two number as input from the user
+2. it performs wrong operation as follows:
++ -> -
+* -> +
+- -> /
+/ -> **
+
+it performs wrong operations 10% of the time, else it performs the right operation.
+*/
