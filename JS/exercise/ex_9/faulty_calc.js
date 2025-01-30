e2b28741c4bcfe175fd14dbaae145095de3b4cd3
nahid784 <nahidhasantusher784@gmail.com>
2025-01-30 20:05:58 +0600

    faulty calculator

--- JS/exercise/ex_9/faulty_calc.js
+++ JS/exercise/ex_9/faulty_calc.js
@@ -9,3 +9,50 @@ it does the following:
 
 it performs wrong operations 10% of the time, else it performs the right operation.
 */
+
+// solution --
+
+let rand = Math.random();
+
+console.log(rand);
+
+let num1 = parseFloat(prompt("Enter the first number: "));
+let num2 = parseFloat(prompt("Enter the second number: "));
+let op = prompt("Enter the operation: ");
+
+if(rand > 0.1){
+    console.log("correct operation");
+    if (op== "+"){
+        console.log("The sum of the numbers is: ", num1 + num2);
+    }
+    else if(op == "-"){
+        console.log("The difference of the numbers is: ", num1 - num2);
+    }
+    else if(op == "*"){
+        console.log("The product of the numbers is: ", num1 * num2);
+    }
+    else if(op == "/"){
+        console.log("The division of the numbers is: ", num1 / num2);
+    }
+    else{
+        console.log("Invalid operation");
+    }
+}
+else{
+    console.log("incorrect answer");
+    if (op== "+"){
+        console.log("The difference of the numbers is: ", num1 - num2);
+    }
+    else if(op == "-"){
+        console.log("The division of the numbers is: ", num1 / num2);
+    }
+    else if(op == "*"){
+        console.log("The sum of the numbers is: ", num1 + num2);
+    }
+    else if(op == "/"){
+        console.log("The power of the numbers is: ", num1 ** num2);
+    }
+    else{
+        console.log("Invalid operation");
+    }
+}
---
+++ JS/exercise/ex_9/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let num1 = prompt("Enter the first number: ");
+let num2 = prompt("Enter the second number: ");
+let op = prompt("Enter the operation: ");
