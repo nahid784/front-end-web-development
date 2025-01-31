9add822f2b364314d522f021b2dfceca556e1439
nahid784 <nahidhasantusher784@gmail.com>
2025-02-01 01:28:36 +0600

    Update arrays.js

--- JS/ch7_arrays/arrays.js
+++ JS/ch7_arrays/arrays.js
@@ -1,6 +1,8 @@
 // array in javascript
 // arrays will be used in react for making numerous component for web development
+// array is mutable or changable in javascript
 
 let arr = [1, 2, 3, 4, 5, 7];
+console.log(arr); // print array
+console.log(arr.length); // length of array
 
-console.log(arr);
