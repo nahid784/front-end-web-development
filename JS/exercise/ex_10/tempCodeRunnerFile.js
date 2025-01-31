31c9ac4ad9ba21684ccc0a299f2a256e2b93ecaa
nahid784 <nahidhasantusher784@gmail.com>
2025-01-31 20:58:53 +0600

    business name generator

--- JS/exercise/ex_10/tempCodeRunnerFile.js
+++ JS/exercise/ex_10/tempCodeRunnerFile.js
@@ -1,16 +1,3 @@
-/*  business name generator. example: canva website
-
-create a business name generator by combining the list of adjectives and shop name and another word
-
-Adjectives: Crazy, Amazing, Fire
-
-Shop Name: Engine, Foods, Garments
-
-Another word: Bros, limited, Hub
-
-Cannot use arrays. can use functions and strings
-*/
-
 function getRandomAdjective() {
     const adjectives = "Crazy Amazing Fire";
     const adjectiveList = adjectives.split(" ");
