ffc31fe5b1a3b7a3708934e59c2faeb2f10691e6
nahid784 <nahidhasantusher784@gmail.com>
2025-02-01 01:35:04 +0600

    Update arrays.js

--- JS/ch7_arrays/arrays.js
+++ JS/ch7_arrays/arrays.js
@@ -3,6 +3,11 @@
 // array is mutable or changable in javascript
 
 let arr = [1, 2, 3, 4, 5, 7];
-console.log(arr); // print array
-console.log(arr.length); // length of array
+console.log("array print:",arr); // print array
+console.log("array length",arr.length); // length of array
+console.log("first element of arary",arr[0]); // print first element of array
+console.log("2nd element of arary",arr[1]); // print first element of array
 
+console.log("first element of array before change",arr[0]); // print first element of array
+arr[0] = 10; // change first element of array
+console.log("first element of array after change",arr[0]); // print first element of array
