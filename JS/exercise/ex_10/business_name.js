4119fe3cb08ee097475d1285529dc65a6792e412
nahid784 <nahidhasantusher784@gmail.com>
2025-01-31 20:25:02 +0600

    business name generator

--- JS/exercise/ex_10/business_name.js
+++ JS/exercise/ex_10/business_name.js
@@ -1,1 +1,12 @@
-// busines name generator
+/*  business name generator. example: canva website
+
+create a business name generator by combining the list of adjectives and shop name and another word
+
+Adjectives: Crazy, Amazing, Fire
+
+Shop Name: Engine, Foods, Garments
+
+Another word: Bros, limited, Hub
+
+Cannot use arrays. can use functions and strings
+*/
