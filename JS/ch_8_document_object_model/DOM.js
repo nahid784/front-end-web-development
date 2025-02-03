7424d3dfd690c271d2891bc862d782d6a066f076
nahid784 <nahidhasantusher784@gmail.com>
2025-02-04 02:12:58 +0600

    DOM

---
+++ JS/ch_8_document_object_model/DOM.js
@@ -0,0 +1,11 @@
+// dom - document obj model is used to map entire html as map
+// it is used to target elements using js
+// interveiw question: bom vs dom
+
+document.title = "Title have changed using js dom"
+console.log(document.title)
+console.log(document.body)  // it is used to change any element of body or to change particular style
+
+document.body.style.backgroundColor = "green";  // it is an inline css
+
+
