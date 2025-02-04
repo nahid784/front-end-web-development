bde608f5df10da98bf76a1b6857894b350be451b
nahid784 <nahidhasantusher784@gmail.com>
2025-02-05 00:38:32 +0600

    Dom parent sibling nodes

--- JS/ch9_dom_manipulation/dom_parent_sibling_nodes.js
+++ JS/ch9_dom_manipulation/dom_parent_sibling_nodes.js
@@ -1,11 +1,27 @@
 // dom - Children, Parent & Sibling Nodes
 console.log("Hello World !")
 
+document.body   // <body>...</body>
+
+document.body.childNodes    // NodeList(7) [text, div.container, text, table, text, script, text]
+
+document.body.childNodes[0] // #text
+
+document.body.childNodes[1] // <div class="container"></div>
+
+document.body.childNodes[1].childNodes  // NodeList(15) [text, comment, text, div.box, text, comment, text, div.box, text, div.box, text, div.box, text, div.box, text]
+
 let cont = document.body.childNodes[1]
-cont.lastElementChild.style.color = "red";
+
+cont.firstChild // #text
+cont.firstElementChild  // <div class="box">box1</div> which is element 
+cont.lastElementChild.style.color = "red";  // text color will be red
 cont.lastElementChild.style.backgroundColor = "green";
 
-document.body.firstElementChild.child
+cont.lastElementChild.parentElement // <div class="container"></div>
+
+document.body.firstElementChild // <div class="container"></div>
+
 document.body.firstElementChild.childNodes  // NodeList(15) [text, comment, text, div.box, text, comment, text, div.box, text, div.box, text, div.box, text, div.box, text] which are all nodes
 
 document.body.firstElementChild.children    // HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box] which are only elements
@@ -18,3 +34,4 @@ document.body.firstElementChild.children[3].nextElementSibling  // <div class="b
 
 document.body.firstElementChild.children[3].previousElementSibling  //<div class="box">box3</div> which is box 3
 
+document.body.children[1].rows  // HTMLCollection(2) [tr, tr]
