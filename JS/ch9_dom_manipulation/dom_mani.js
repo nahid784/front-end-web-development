036c8da76f8346044e4fbe745b9ab61d5691d5c4
nahid784 <nahidhasantusher784@gmail.com>
2025-02-05 00:08:42 +0600

    dom manipulation

--- JS/ch9_dom_manipulation/dom_mani.js
+++ JS/ch9_dom_manipulation/dom_mani.js
@@ -1,2 +1,6 @@
 // dom - Children, Parent & Sibling Nodes
+console.log("Hello World !")
 
+let cont = document.body.childNodes[1]
+cont.lastElementChild.style.color = "red";
+cont.lastElementChild.style.backgroundColor = "green";
