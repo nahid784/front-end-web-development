76ca57de3f5f4a6e36d31042b14e839b7e3d6ac1
nahid784 <nahidhasantusher784@gmail.com>
2025-02-05 00:16:55 +0600

    Update dom_mani.js

--- JS/ch9_dom_manipulation/dom_mani.js
+++ JS/ch9_dom_manipulation/dom_mani.js
@@ -5,4 +5,9 @@ let cont = document.body.childNodes[1]
 cont.lastElementChild.style.color = "red";
 cont.lastElementChild.style.backgroundColor = "green";
 
-document.body.firstElementChild
+document.body.firstElementChild.child
+document.body.firstElementChild.childNodes  // NodeList(15) [text, comment, text, div.box, text, comment, text, div.box, text, div.box, text, div.box, text, div.box, text] which are all nodes
+
+document.body.firstElementChild.children    // HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box] which are only elements
+
+document.body.firstElementChild.children[0]     // div.box
