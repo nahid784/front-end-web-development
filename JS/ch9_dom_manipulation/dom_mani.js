08088c8af4070b2f2cf7e5c035576dbe29e28950
nahid784 <nahidhasantusher784@gmail.com>
2025-02-05 00:21:46 +0600

    Update dom_mani.js

--- JS/ch9_dom_manipulation/dom_mani.js
+++ JS/ch9_dom_manipulation/dom_mani.js
@@ -11,3 +11,10 @@ document.body.firstElementChild.childNodes  // NodeList(15) [text, comment, text
 document.body.firstElementChild.children    // HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box] which are only elements
 
 document.body.firstElementChild.children[0]     // div.box
+
+document.body.firstElementChild.children[1]     // <div class="box">box2</div>
+
+document.body.firstElementChild.children[3].nextElementSibling  // <div class="box" style="color: red; background-color: green;">box5</div> which in box 5
+
+document.body.firstElementChild.children[3].previousElementSibling  //<div class="box">box3</div> which is box 3
+
