d0594cf392328b76a750843a9784d4a9cc4cbfd1
nahid784 <nahidhasantusher784@gmail.com>
2025-02-05 22:04:25 +0600

    id class selector using dom

--- JS/ch10_selecting_id_classes/id_class_selector.js
+++ JS/ch10_selecting_id_classes/id_class_selector.js
@@ -11,3 +11,38 @@ boxes[2].style.backgroundColor = 'blue'
 // targeting using id
 document.getElementById("red").style.backgroundColor = 'red'
 // this will target the element that have red id included
+
+// query selector
+document.querySelector(".box").style.backgroundColor = "green"
+// it will select only the first element that will have the class name
+
+// to select all elements of the class
+console.log(document.querySelectorAll(".box"))  // for printing
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     console.log(e)
+//     e.style.backgroundColor = "yellow"
+// }) // to print and add style one by one
+
+// elements by tagname
+document.getElementsByTagName("div")    // HTMLCollection(9) [div.container, div.box, div.box, div.box, div#red.box, div.box, div.box, div.box, div, red: div#red.box]
+// it will return html collection
+
+//matches
+e = document.getElementsByTagName("div")
+e[4].matches("#red")
+// to check if the selector matches or not
+
+//closest
+// to look for the nearest ancestor that matches the user input given css selector
+e[4].closest("#red") // <div id="red" class="box" style="background-color: red;">Nahid Hasan 3</div>
+e[4].closest(".container")  // <div class="container">..</div>
+
+// contains - returns true if element b is inside of element a or element b is element a
+document.querySelector(".container").contains(e[2]) // true
+
+document.querySelector(".container").contains(document.querySelector("body"))   // false
+// body is not in container
+
+document.querySelector("body").contains(document.querySelector(".container"))   // true
+// container is in body
