e5b7c223a914602a0c672d0e0ace79603832ff5c
nahid784 <nahidhasantusher784@gmail.com>
2025-02-08 23:30:08 +0600

    Update color_the_boxes.js

--- JS/exercise/ex_12/color_the_boxes.js
+++ JS/exercise/ex_12/color_the_boxes.js
@@ -8,15 +8,16 @@ let boxes = document.querySelector(".container").children
 console.log(boxes)
 
 function getRandomColor(){
-    let val1 = 233;
-    let val2 = 125;
-    let val3 = 111;
+    let val1 = Math.ceil(0 + Math.random() * 255);
+    let val2 = Math.ceil(0 + Math.random() * 255);
+    let val3 = Math.ceil(0 + Math.random() * 255);
     return `rgb(${val1}, ${val2}, ${val3})`
     
 }
 
 Array.from(boxes).forEach(e=>{ // make array using array, cause in html collection foreach cant be added
     e.style.backgroundColor = getRandomColor()
+    e.style.color = getRandomColor()
     console.log(e)
 })
 
