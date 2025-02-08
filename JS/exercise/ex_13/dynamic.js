447c1d8b67338e417f4469e1ae02d1d4417918bf
nahid784 <nahidhasantusher784@gmail.com>
2025-02-09 01:59:16 +0600

    dynamic site builder

---
+++ JS/exercise/ex_13/dynamic.js
@@ -0,0 +1,8 @@
+function createcard(title, cname, views, monthsold, duration, thumbnail ){
+    // finish the function
+}
+
+// sample card
+createcard("Introduction to backend | Signma web dev video #2", "CodeWithHarry", 56000, 7, "31:32", "link")
+
+// need to use styles on the container
