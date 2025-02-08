4f13db652a4f26101bbab795fdaa42a75bbc4a5d
nahid784 <nahidhasantusher784@gmail.com>
2025-02-08 21:09:23 +0600

    Update color_the_boxes.js

---
+++ JS/exercise/ex_12/color_the_boxes.js
@@ -0,0 +1,8 @@
+console.log("colors the boxes")
+
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+
+Array.from(boxes).forEach(e=>{ // make array using array, cause in html collection foreach cant be added
+    console.log(e)
+})
