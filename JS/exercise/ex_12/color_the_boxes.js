d96c6037769377e977c27d223c5bb31a3c5571d9
nahid784 <nahidhasantusher784@gmail.com>
2025-02-08 23:40:25 +0600

    Update color_the_boxes.js

--- JS/exercise/ex_12/color_the_boxes.js
+++ JS/exercise/ex_12/color_the_boxes.js
@@ -1,6 +1,6 @@
 console.log("colors the boxes")
 
-// let boxes = document.getElementsByClassName("box")
+// let boxes = document.getElementsByClassName(".box")
 
 // 2nd method
 let boxes = document.querySelector(".container").children
@@ -11,7 +11,7 @@ function getRandomColor(){
     let val1 = Math.ceil(0 + Math.random() * 255);
     let val2 = Math.ceil(0 + Math.random() * 255);
     let val3 = Math.ceil(0 + Math.random() * 255);
-    // math.ceil is provide the next integer of the float value
+    // math.ceil is provide the next integer of the float values
     return `rgb(${val1}, ${val2}, ${val3})`
     
 }
@@ -23,4 +23,24 @@ Array.from(boxes).forEach(e=>{ // make array using array, cause in html collecti
 })
 
 
+// hex method
 
+// var boxes = document.querySelectorAll(".box");
+
+// for(var i = 0; i < boxes.length; i++){
+//     var box = boxes[i];
+
+//     // Generate random color (16777215 is white in Decimal) 
+//     var randomcColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+//     // Generate random background  color
+//     var randomBackgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+//     // Assing random color and background
+//     box.style.color = randomcColor;
+//     box.style.backgroundColor = randomBackgroundColor;
+
+//     // set content to show color code
+//     box.textContent = randomcColor;
+
+// }
