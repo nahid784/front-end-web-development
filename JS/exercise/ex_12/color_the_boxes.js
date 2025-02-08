8a6f549261494ee05645cae1f19c75c51f068b0c
nahid784 <nahidhasantusher784@gmail.com>
2025-02-08 23:22:51 +0600

    Update color_the_boxes.js

--- JS/exercise/ex_12/color_the_boxes.js
+++ JS/exercise/ex_12/color_the_boxes.js
@@ -1,10 +1,13 @@
 console.log("colors the boxes")
 
-let boxes = document.getElementsByClassName("box")
-console.log(boxes)
+// let boxes = document.getElementsByClassName("box")
 
 // 2nd method
 // let box = document.querySelector(."container").children
+
+console.log(boxes)
+
+
 
 Array.from(boxes).forEach(e=>{ // make array using array, cause in html collection foreach cant be added
     console.log(e)
