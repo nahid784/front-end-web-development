94a09afc2c1b2920ecc1673a3fa8d42c8776fb9f
nahid784 <nahidhasantusher784@gmail.com>
2025-02-09 23:58:00 +0600

    Update dom_insert_delete.js

--- JS/ch11_insert_&_remove/dom_insert_delete.js
+++ JS/ch11_insert_&_remove/dom_insert_delete.js
@@ -4,3 +4,7 @@ document.querySelector(".box")  // <div class="box">This is a box</div>
 
 document.querySelector(".box").innerHTML    // 'This is a box'
 
+document.querySelector(".container").innerHTML  // '\n        <div class="box">This is a box</div>\n    '
+
+document.querySelector(".box").innerText    // 'This is a box'
+
