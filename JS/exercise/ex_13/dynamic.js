3e45a0fa7bb0fd21a3fae19eeaebbb4215f68545
nahid784 <nahidhasantusher784@gmail.com>
2025-02-11 02:00:43 +0600

    Update dynamic.js

--- JS/exercise/ex_13/dynamic.js
+++ JS/exercise/ex_13/dynamic.js
@@ -1,8 +1,15 @@
-function createcard(title, cname, views, monthsold, duration, thumbnail ){
-    // finish the function
+// function createcard(title, cname, views, monthsold, duration, thumbnail ){
+//     // finish the function
+// }
+
+// // sample card
+// createcard("Introduction to backend | Signma web dev video #2", "CodeWithHarry", 56000, 7, "31:32", "link")
+
+// // need to use styles on the container
+
+function createcard(){
+    const card = document.createElement("div");
+    card.innerHTML = "Title";
+    div.setAttribute("class", "created");
+    document.querySelector(".container").append(div);
 }
-
-// sample card
-createcard("Introduction to backend | Signma web dev video #2", "CodeWithHarry", 56000, 7, "31:32", "link")
-
-// need to use styles on the container
