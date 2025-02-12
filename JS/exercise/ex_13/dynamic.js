96c2002b1e8eeaed6052f3ec0161a4a97aaab0f2
nahid784 <nahidhasantusher784@gmail.com>
2025-02-12 20:38:59 +0600

    Update dynamic.js

--- JS/exercise/ex_13/dynamic.js
+++ JS/exercise/ex_13/dynamic.js
@@ -8,5 +8,26 @@
 // // need to use styles on the container
 
 function createCard(title, cname, views, monthsold, duration, thumb){
-    let html =  ``
+    let viewnum
+    if(views<1000000){
+        viewnum = views/1000 + "K";
+    }
+    else if(views>1000000){
+        viewnum = views/1000000 + "M";
+    }
+    else{
+        viewnum = views/1000 + "K";
+    }
+    let html =  `<div class="card">
+        <div class="image">
+          <img
+            src="${thumb}"
+            alt=""
+          />
+        </div>
+        <div class="text">
+          <h1>${title}</h1>
+          <p>${cname} . ${viewnum} views . ${monthsold} Months ago</p>
+        </div>
+      </div>`
 }
