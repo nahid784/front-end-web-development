d2e429f4d5c3eab69935218cf8ced3a1bf3d501f
nahid784 <nahidhasantusher784@gmail.com>
2025-02-15 00:13:45 +0600

    event handler

--- JS/ch12_events/events.js
+++ JS/ch12_events/events.js
@@ -19,6 +19,10 @@ button1.addEventListener("dblclick",()=>{
     // to select one element we use element selector.
 })
 
+button.addEventListener("contextmenu", ()=>{
+    alert("Dont hack by clicking right click !")
+})
+
 document.addEventListener("keydown",(e)=>{
     console.log(e)
     // key listner, button is recorded when any key is pressed
@@ -26,3 +30,24 @@ document.addEventListener("keydown",(e)=>{
     console.log(e.key, e.keyCode)
     
 }) 
+
+// set Interval & timeout
+// i want an element to run repeatedly for every 2 sec
+
+function getrandom(){
+    let val1 = Math.ceil(0 + Math.random() * 255);
+    let val2 = Math.ceil(0 + Math.random() * 255);
+    let val3 = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+let a = setInterval(() => {
+    document.querySelector(".buttn").style.background = getrandom()
+}, 3000);
+// it will change bg color in every 3 second
+console.log(a)
+
+let b = setTimeout(() => {
+    document.querySelector(".buttn1").style.background = getrandom()
+}, 1000);
+// it will change after certain amount of time (3sec)
+console.log(b)
