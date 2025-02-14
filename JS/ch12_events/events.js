7cfc88a60ef6d2bb76a9d5b954504a57311e890c
nahid784 <nahidhasantusher784@gmail.com>
2025-02-14 21:03:32 +0600

    Create events.js

---
+++ JS/ch12_events/events.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("btn") 
+// it took the button id from html 
+
+button.addEventListener("click",()=>{
+      alert("The button is clicked !")
+    //   when the button is clicked, it will show this alert.
+})
