5f380fc89569eecf3552d9974032c2ad336d8355
nahid784 <nahidhasantusher784@gmail.com>
2025-02-16 01:16:34 +0600

    Create callback&Promises.js

---
+++ JS/ch13_callback&Promises/callback&Promises.js
@@ -0,0 +1,10 @@
+// Assynchronus nature of JS
+console.log("This is first line")
+console.log("This is 2nd line")
+
+setTimeout(()=>{
+    console.log("this is third line, but comes last because of Assynchronus nature of js")
+}, 2000);
+
+console.log("this is last line")
+
