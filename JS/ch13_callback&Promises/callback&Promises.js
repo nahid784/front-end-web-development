a085872ff2f0367c855c9265e7df02ee00b9551c
nahid784 <nahidhasantusher784@gmail.com>
2025-02-16 01:36:12 +0600

    Update callback&Promises.js

--- JS/ch13_callback&Promises/callback&Promises.js
+++ JS/ch13_callback&Promises/callback&Promises.js
@@ -1,10 +1,17 @@
-// Assynchronus nature of JS
+// Assynchronus prog nature of JS
 console.log("This is first line")
 console.log("This is 2nd line")
 
-setTimeout(()=>{
+setTimeout(() => {
     console.log("this is third line, but comes last because of Assynchronus nature of js")
 }, 2000);
 
 console.log("this is last line")
+
+
+// CallBack Function
+
+const loadScript = (src, callBack) => {
+  let sc = document.createElement("script");
+}
 
