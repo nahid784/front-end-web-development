3aece7a08e0ba8301a0580acbf95843eb10e5785
nahid784 <nahidhasantusher784@gmail.com>
2025-02-23 23:46:46 +0600

    Update callback&Promises.js

--- JS/ch13_callback&Promises/callback&Promises.js
+++ JS/ch13_callback&Promises/callback&Promises.js
@@ -6,8 +6,8 @@ setTimeout(() => {
     console.log("this is third line, but comes last because of Assynchronus nature of js")
 }, 2000);
 
-console.log("this is last line")
 
+console.log("this is last line")
 
 // CallBack Function
 
