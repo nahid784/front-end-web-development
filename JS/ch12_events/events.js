030846bc1b9dd7cc29e1c212c7ac04a05817c337
nahid784 <nahidhasantusher784@gmail.com>
2025-02-26 22:57:36 +0600

    Update events.js

--- JS/ch12_events/events.js
+++ JS/ch12_events/events.js
@@ -1,40 +1,40 @@
 // list of mouse events: https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
-let button = document.getElementById("btn") 
+let button = document.getElementById("btn")
 // it took the button id from html 
 
-button.addEventListener("click",()=>{
-      alert("The button is clicked !")
+button.addEventListener("click", () => {
+    alert("The button is clicked !")
     //   when the button is clicked, it will show this alert.
-    document.querySelector(".box").innerHTML = 
-    "<b>Text has been changed after clicking the button</b>"
+    document.querySelector(".box").innerHTML =
+        "<b>Text has been changed after clicking the button</b>"
     // to select one element we use element selector.
 })
 
 let button1 = document.getElementById("btn1");
-button1.addEventListener("dblclick",()=>{
-      alert("The button is double clicked !")
+button1.addEventListener("dblclick", () => {
+    alert("The button is double clicked !")
     //   when the button is clicked, it will show this alert.
-    document.querySelector(".box").innerHTML = 
-    "<b>Text has been changed again after double clicking the button</b>"
+    document.querySelector(".box").innerHTML =
+        "<b>Text has been changed again after double clicking the button</b>"
     // to select one element we use element selector.
 })
 
-button.addEventListener("contextmenu", ()=>{
+button.addEventListener("contextmenu", () => {
     alert("Dont hack by clicking right click !")
 })
 
-document.addEventListener("keydown",(e)=>{
+document.addEventListener("keydown", (e) => {
     console.log(e)
     // key listner, button is recorded when any key is pressed
-    
+
     console.log(e.key, e.keyCode)
-    
-}) 
+
+})
 
 // set Interval & timeout
 // i want an element to run repeatedly for every 2 sec
 
-function getrandom(){
+function getrandom() {
     let val1 = Math.ceil(0 + Math.random() * 255);
     let val2 = Math.ceil(0 + Math.random() * 255);
     let val3 = Math.ceil(0 + Math.random() * 255);
