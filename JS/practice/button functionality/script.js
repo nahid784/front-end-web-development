b581863d69be7dccc3c9a63637b254fdc7bab445
nahid784 <nahidhasantusher784@gmail.com>
2025-03-02 16:34:22 +0600

    Create script.js

---
+++ JS/practice/button functionality/script.js
@@ -0,0 +1,8 @@
+let original_text = document.getElementById('des').innerHTML
+function content(){
+    document.getElementById('des').innerHTML = "Line changed after clicking button"
+}
+
+function reset(){
+    document.getElementById('des').innerHTML = original_text;
+}
