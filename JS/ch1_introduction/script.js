8b630bf89309c4157287682b671b983541828e16
nahid784 <nahidhasantusher784@gmail.com>
2025-03-03 23:53:57 +0600

    Update script.js

--- JS/ch1_introduction/script.js
+++ JS/ch1_introduction/script.js
@@ -1,19 +1,18 @@
-alert("Hello World");   // it will show alert box
+alert("Hello World"); // it will show alert box
 
-console.log("Hey It is a console..") // it is used for print in js
+console.log("Hey It is a console.."); // it is used for print in js
 
-var a = prompt("Enter your number:");   // variable declaration
-var istrue = confirm("this is a confirmation prompt") // confirmation popup start
-if(istrue){
-  console.log("confirmation is true")
-}
-else{
-  console.log("confirmation is false")
+var a = prompt("Enter your number:"); // variable declaration
+var istrue = confirm("this is a confirmation prompt"); // confirmation popup start
+if (istrue) {
+  console.log("confirmation is true");
+} else {
+  console.log("confirmation is false");
 }
 // confirmation popup end
 
 console.log("your number is " + a);
 
-document.title = "Webpage title"  // added document title 
+document.title = "Webpage title"; // added document title
 
-document.body.style.backgroundColor = "red"   // added background color as inline css
+document.body.style.backgroundColor = "red"; // added background color as inline css
