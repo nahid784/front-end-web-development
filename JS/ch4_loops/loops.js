369083a094f6687b46a77fbed4b1da6ffcdbcfcb
nahid784 <nahidhasantusher784@gmail.com>
2025-03-03 23:53:12 +0600

    Update loops.js

--- JS/ch4_loops/loops.js
+++ JS/ch4_loops/loops.js
@@ -10,54 +10,53 @@ console.log(a + 2);
 
 // for loop
 for (let i = 0; i < 100; i++) {
-    console.log(a + i);
+  console.log(a + i);
 }
 
 // for in loop
 // it is used for object
 let obj = {
-    name: "Nahid",
-    role: "Developer",
-    company: "Google"
-}
+  name: "Nahid",
+  role: "Developer",
+  company: "Google",
+};
 
-console.log("normal for in loop:")
+console.log("normal for in loop:");
 for (const key in obj) {
+  const element = obj[key];
+  console.log(key, element);
+}
+console.log("\n");
+
+console.log("Standard for in loop:");
+for (const key in obj) {
+  if (Object.prototype.hasOwnProperty.call(obj, key)) {
     const element = obj[key];
-    console.log(key, element)
-
+    console.log(key, element);
+  }
 }
-console.log("\n")
-
-console.log("Standard for in loop:")
-for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        console.log(key, element)
-    }
-}
-console.log("\n")
+console.log("\n");
 
 // for-off loop
 // its used for array and string
 for (const char of "Nahid") {
-    console.log(char);
+  console.log(char);
 }
 
 // while loop
-console.log("while loop:")
+console.log("while loop:");
 i = 0;
 while (i > 6) {
-    console.log(i);
-    i++;
+  console.log(i);
+  i++;
 }
 
 // do while loop
 // used for at least one time
 // will check the condition after the first iteration
-console.log("do-while loop:")
-let y = 0
+console.log("do-while loop:");
+let y = 0;
 do {
-    console.log(y);
-    y++;
+  console.log(y);
+  y++;
 } while (y < 6);
