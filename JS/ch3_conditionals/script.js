05f08f41887a443202f8085d2fd883b3cabbbdc8
nahid784 <nahidhasantusher784@gmail.com>
2025-03-03 23:53:31 +0600

    Update script.js

--- JS/ch3_conditionals/script.js
+++ JS/ch3_conditionals/script.js
@@ -1,4 +1,4 @@
-console.log("It is a conditional if else of js")
+console.log("It is a conditional if else of js");
 
 let age = 17;
 let grace = 2;
@@ -12,48 +12,43 @@ console.log("Power of", age, "^", grace, "is", age ** grace);
 console.log("Remainder or Modulus is ", age % grace);
 
 // Assignment Operators
-age += grace;   // age = age + grace;
+age += grace; // age = age + grace;
 console.log("Updated Age is ", age);
 
-age -= grace;   // age = age - grace;
+age -= grace; // age = age - grace;
 console.log("Updated Age is ", age);
 
 // with grace
 if (age + grace > 18) {
-    console.log("you can drive");
-}
-else {
-    console.log("you cant drive");
+  console.log("you can drive");
+} else {
+  console.log("you cant drive");
 }
 
 // without grace
 if (age > 18) {
-    console.log("you can drive");
-}
-else {
-    console.log("you cant drive")
+  console.log("you can drive");
+} else {
+  console.log("you cant drive");
 }
 
 // equal to
 if (age == age) {
-    console.log("age is equal");
+  console.log("age is equal");
 }
 
 // "3" == 3, true (this compares only value)
 // "3" === 3. false (this compares value and datatype)
 
-
 // if, else-if, else statement
 // these three works together
 // if one of them is true then others will not work or run
 if (age != 17) {
-    console.log("only if");
-}
-else if (age == 0) {
-    console.log("you are wrong");
-}
-else {
-    console.log("you cant drive")
+  console.log("only if");
+} else if (age == 0) {
+  console.log("you are wrong");
+} else {
+  console.log("you cant drive");
 }
 // we can use multiple else-if statements
 
@@ -61,7 +56,7 @@ else {
 // condition ? true : false
 a = 10;
 b = 8;
-let c = a > b ? (a - b) : (b - a);
+let c = a > b ? a - b : b - a;
 console.log("Ternary Operator is ", c);
 // this translates to
 // if a is greater than b then(?) a - b else(:) b - a
