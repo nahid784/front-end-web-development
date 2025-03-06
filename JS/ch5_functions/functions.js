8ed448533f3998bfed63dafbf892d7312c2e5e25
nahid784 <nahidhasantusher784@gmail.com>
2025-03-06 21:26:19 +0600

    Update functions.js

--- JS/ch5_functions/functions.js
+++ JS/ch5_functions/functions.js
@@ -2,33 +2,33 @@ console.log("Functions Tutorial");
 
 // before function
 console.log("Before function");
-console.log("Hello Nahid, how are you?")
-console.log("Hello Nahid, where are you going?")
-console.log("Hello Nahid, how is your day?")
-console.log("Hello Nahid, what are u doing?")
+console.log("Hello Nahid, how are you?");
+console.log("Hello Nahid, where are you going?");
+console.log("Hello Nahid, how is your day?");
+console.log("Hello Nahid, what are u doing?");
 
 // after function
 console.log("After function");
 function nice(name) {
-    console.log("Hello " + name + ", how are you?")
-    console.log("Hello " + name + ", where are you going?")
-    console.log("Hello " + name + ", how is your day?")
-    console.log("Hello " + name + ", what are u doing?")
+  console.log("Hello " + name + ", how are you?");
+  console.log("Hello " + name + ", where are you going?");
+  console.log("Hello " + name + ", how is your day?");
+  console.log("Hello " + name + ", what are u doing?");
 }
 // function is used to avoid the repetition of code
-nice("Nahid"); 
+nice("Nahid");
 nice("Rahim");
 
 // sum function
 function sums(a, b) {
-    console.log(a + b);
+  console.log(a + b);
 }
 
 sums(4, 2);
 
 function sum(a, b) {
-    return a + b;
-    // it is used to return the value of the function
+  return a + b;
+  // it is used to return the value of the function
 }
 
 result1 = sum(3, 2);
@@ -39,11 +39,10 @@ console.log("Sum of the numbers is: ", result1);
 console.log("Sum of the numbers is: ", result2);
 console.log("Sum of the numbers is: ", result3);
 
-
 // arrow function
-const func1 = (x)=>{
-    console.log("Hello", x);
-}
+const func1 = (x) => {
+  console.log("Hello", x);
+};
 
 func1("34");
 func1("Nahid");
