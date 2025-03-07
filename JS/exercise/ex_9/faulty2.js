27d1be89d8fa82d2a9e2fb769092ae3b52969e92
nahid784 <nahidhasantusher784@gmail.com>
2025-03-07 21:10:40 +0600

    Update faulty2.js

--- JS/exercise/ex_9/faulty2.js
+++ JS/exercise/ex_9/faulty2.js
@@ -13,31 +13,33 @@ It performs wrong operation 10% of the times
 
 */
 
-let random = Math.random()
-console.log(random)
-let a = prompt("Enter first number")
-let c = prompt("Enter operation")
-let b = prompt("Enter second number")
+// Generate a random number between 0 and 1
+let random = Math.random();
+console.log(random); // Log the random number for debugging
 
+// Prompt user to enter first number
+let a = prompt("Enter first number");
+
+// Prompt user to enter an operation (+, -, *, /)
+let c = prompt("Enter operation");
+
+// Prompt user to enter second number
+let b = prompt("Enter second number");
+
+// Define an object that maps correct operations to faulty ones
 let obj = {
-    "+": "-",
-    "*": "+",
-    "-": "/",
-    "/": "**",
-}
+  "+": "-",
+  "*": "+",
+  "-": "/",
+  "/": "**", // Exponentiation instead of division
+};
 
-
-
+// Condition to determine whether to perform a correct or faulty operation
 if (random > 0.1) {
-    // Perform correct calculation
-    console.log(`The result is ${a} ${c} ${b}`)
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
-    
-}
-
-else {
-    // Perform wrong calculation
-    c = obj[c]
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
-
+  // 90% of the time, perform the correct calculation
+  alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+} else {
+  // 10% of the time, perform a faulty calculation by replacing the operation
+  c = obj[c]; // Replace operation with the faulty one
+  alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
 }
