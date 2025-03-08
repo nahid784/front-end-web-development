940694c4e34c14be67072bd3bb2d3a48cf71e335
nahid784 <nahidhasantusher784@gmail.com>
2025-03-08 21:33:49 +0600

    Update business_name2.js

--- JS/exercise/ex_10/business_name2.js
+++ JS/exercise/ex_10/business_name2.js
@@ -15,35 +15,29 @@ let rand = Math.random();
 let first, second, third;
 // 0 0.33 0.66 1
 if (rand < 0.33) {
-    first = "Crazy";
-}
-else if (rand < 0.66 && rand >= 0.33) {
-    first = "Amazing";
-}
-else {
-    first = "Fire";
+  first = "Crazy";
+} else if (rand < 0.66 && rand >= 0.33) {
+  first = "Amazing";
+} else {
+  first = "Fire";
 }
 
-rand= Math.random();
+rand = Math.random();
 if (rand < 0.33) {
-    second = "Engine";
-}
-else if (rand < 0.66 && rand >= 0.33) {
-    second = "Foods";
-}
-else {
-    second = "Garments";
+  second = "Engine";
+} else if (rand < 0.66 && rand >= 0.33) {
+  second = "Foods";
+} else {
+  second = "Garments";
 }
 
-rand= Math.random();
+rand = Math.random();
 if (rand < 0.33) {
-    third = "Bros";
-}
-else if (rand < 0.66 && rand >= 0.33) {
-    third = "limited";
-}
-else {
-    third = "Hub";
+  third = "Bros";
+} else if (rand < 0.66 && rand >= 0.33) {
+  third = "limited";
+} else {
+  third = "Hub";
 }
 
 console.log(`${first} ${second} ${third}`);
