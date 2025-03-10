67aacbbfaff7af56e9bffd13669824acfffea0d7
nahid784 <nahidhasantusher784@gmail.com>
2025-03-10 23:43:30 +0600

    Update factorial.js

--- JS/exercise/ex_11/factorial.js
+++ JS/exercise/ex_11/factorial.js
@@ -1,13 +1,13 @@
-// calculate the factorial of a number using reduce and for loops 
+// calculate the factorial of a number using reduce and for loops
 
 let num = prompt("Enter a number: ");
 let arr = [];
 for (let i = num; i >= 1; i--) {
-    arr.push(i);
+  arr.push(i);
 }
 
 const fact = (a, b) => {
-    return a * b;
-}
+  return a * b;
+};
 
-console.log("factorial of",num, "is:",arr.reduce(fact)); // 120
+console.log("factorial of", num, "is:", arr.reduce(fact)); // 120
