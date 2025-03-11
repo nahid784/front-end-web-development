cf710cd3cfa57dd91786b662424de09e73c1a050
nahid784 <nahidhasantusher784@gmail.com>
2025-03-11 22:15:33 +0600

    Update dynamic.js

--- JS/exercise/ex_13/dynamic.js
+++ JS/exercise/ex_13/dynamic.js
@@ -7,18 +7,16 @@
 
 // // need to use styles on the container
 
-function createCard(title, cname, views, monthsold, duration, thumb){
-    let viewnum
-    if(views<1000000){
-        viewnum = views/1000 + "K";
-    }
-    else if(views>1000000){
-        viewnum = views/1000000 + "M";
-    }
-    else{
-        viewnum = views/1000 + "K";
-    }
-    let html =  `<div class="card">
+function createCard(title, cname, views, monthsold, duration, thumb) {
+  let viewnum;
+  if (views < 1000000) {
+    viewnum = views / 1000 + "K";
+  } else if (views > 1000000) {
+    viewnum = views / 1000000 + "M";
+  } else {
+    viewnum = views / 1000 + "K";
+  }
+  let html = `<div class="card">
         <div class="image">
           <img
             src="${thumb}"
@@ -30,9 +28,17 @@ function createCard(title, cname, views, monthsold, duration, thumb){
           <h1>${title}</h1>
           <p>${cname} . ${viewnum} views . ${monthsold} Months ago</p>
         </div>
-      </div>`
+      </div>`;
 
-      document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html;
+  document.querySelector(".container").innerHTML =
+    document.querySelector(".container").innerHTML + html;
 }
 
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 56000, 8, "24:12", "https://i.ytimg.com/vi/UzYRQURh_pY/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLCuvVlRNxvKcLMpdtUybkODOxsbjw")
+createCard(
+  "Introduction to Backend | Sigma Web Dev video #2",
+  "CodeWithHarry",
+  56000,
+  8,
+  "24:12",
+  "https://i.ytimg.com/vi/UzYRQURh_pY/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLCuvVlRNxvKcLMpdtUybkODOxsbjw"
+);
