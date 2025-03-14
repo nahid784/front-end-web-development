89de80d6f2f56279c245d6b06f99eed74c4a074b
nahid784 <nahidhasantusher784@gmail.com>
2025-03-14 23:55:06 +0600

    Update EventBubbling.js

--- JS/ch12_events/EventBubbling.js
+++ JS/ch12_events/EventBubbling.js
@@ -3,43 +3,42 @@
 
 // Bubbling
 document.querySelector(".child").addEventListener("click", () => {
-    alert("Child div was clicked !")
-})
+  alert("Child div was clicked !");
+});
 
 document.querySelector(".childContainer").addEventListener("click", () => {
-    alert("Child Container div was clicked !")
-})
+  alert("Child Container div was clicked !");
+});
 
 document.querySelector(".container").addEventListener("click", () => {
-    alert("Container div was clicked !")
-})
+  alert("Container div was clicked !");
+});
 
 // after stopping bubbling
 document.querySelector(".child1").addEventListener("click", (e) => {
-    e.stopPropagation()
-    alert("Child div was clicked !")
-})
+  e.stopPropagation();
+  alert("Child div was clicked !");
+});
 
-document.querySelector(".childContainer1").addEventListener("click", (e /* we can take event object here */) => {
-    e.stopPropagation   // to stop event bubbling
-    alert("Child Container div was clicked !")
-})
+document
+  .querySelector(".childContainer1")
+  .addEventListener("click", (e /* we can take event object here */) => {
+    e.stopPropagation; // to stop event bubbling
+    alert("Child Container div was clicked !");
+  });
 
 document.querySelector(".container1").addEventListener("click", () => {
-    alert("Container div was clicked !")
-})
+  alert("Container div was clicked !");
+});
 
 // set Interval & timeout
 // i want an element to run repeatedly for every 2 sec
 function getrandom() {
-    let val1 = Math.ceil(0 + Math.random() * 255);
-    let val2 = Math.ceil(0 + Math.random() * 255);
-    let val3 = Math.ceil(0 + Math.random() * 255);
-    return `rgb(${val1}, ${val2}, ${val3})`
+  let val1 = Math.ceil(0 + Math.random() * 255);
+  let val2 = Math.ceil(0 + Math.random() * 255);
+  let val3 = Math.ceil(0 + Math.random() * 255);
+  return `rgb(${val1}, ${val2}, ${val3})`;
 }
 setInterval(() => {
-    document.querySelector(".childContainer").style.background = getrandom()
+  document.querySelector(".childContainer").style.background = getrandom();
 }, 3000);
-
-
-
