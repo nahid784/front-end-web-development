694a43fa1ac8abb08bb5e5460e39e3854ef6b279
nahid784 <nahidhasantusher784@gmail.com>
2025-03-14 23:54:51 +0600

    Update dom_insert_delete.js

--- JS/ch11_insert_&_remove/dom_insert_delete.js
+++ JS/ch11_insert_&_remove/dom_insert_delete.js
@@ -1,71 +1,70 @@
 // dom insert and delete
 // document.designMode = "on", to design any website
 
-document.querySelector(".box")  // <div class="box">This is a box</div>
+document.querySelector(".box"); // <div class="box">This is a box</div>
 
-document.querySelector(".box").innerHTML    // 'This is a box'
+document.querySelector(".box").innerHTML; // 'This is a box'
 
-document.querySelector(".container").innerHTML  // '\n        <div class="box">This is a box</div>\n    '
+document.querySelector(".container").innerHTML; // '\n        <div class="box">This is a box</div>\n    '
 
-document.querySelector(".box").innerText    // 'This is a box'
+document.querySelector(".box").innerText; // 'This is a box'
 
-document.querySelector(".container").innerText    // 'This is a box'
+document.querySelector(".container").innerText; // 'This is a box'
 
-
-document.querySelector(".container").innerText
+document.querySelector(".container").innerText;
 // 'This is a box\nThis is a box'
 
-document.querySelector(".container").outerHTML
-// <div class="container">\n        <div class="box">This is a box</div>\n        <div class="box">This is a box</div>\n  
-// this container also be shown using this 
+document.querySelector(".container").outerHTML;
+// <div class="container">\n        <div class="box">This is a box</div>\n        <div class="box">This is a box</div>\n
+// this container also be shown using this
 
-document.querySelector(".container").tagName
+document.querySelector(".container").tagName;
 // 'DIV'
 
-document.querySelector(".container").nodeName
+document.querySelector(".container").nodeName;
 // 'DIV'
 // tagname is applicable for element and nodename is applicable for any node
 
-document.querySelector(".container").textContent
-// '\n        This is a box\n        This is a box\n  
+document.querySelector(".container").textContent;
+// '\n        This is a box\n        This is a box\n
 
-document.querySelector(".container").hidden
+document.querySelector(".container").hidden;
 // false
 
 // document.querySelector(".container").hidden = true
 // true
 // container will be hidden
 
-document.querySelector(".box").innerHTML = "Hey the line has changed"
+document.querySelector(".box").innerHTML = "Hey the line has changed";
 // it will select the first element to change
 
-document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").hasAttribute("style");
 // false
 
-document.querySelector(".box1").hasAttribute("style")
+document.querySelector(".box1").hasAttribute("style");
 // true, it means it has style in it.
 
-document.querySelector(".box1").getAttribute("style")
+document.querySelector(".box1").getAttribute("style");
 // 'display: flex;', it shows which attribute it contains
 
-document.querySelector(".box1").setAttribute("style", "display: inline")
+document.querySelector(".box1").setAttribute("style", "display: inline");
 // it will set the attribute according to the command
 
-document.querySelector(".box1").attributes
+document.querySelector(".box1").attributes;
 // NamedNodeMap {0: class, 1: style, class: class, style: style, length: 2}, it will show all the attributes
 
-document.querySelector(".box1").removeAttribute("style")
+document.querySelector(".box1").removeAttribute("style");
 // style will be removed
 
 document.designMode = "on";
-// any website can be designed on the page. 
+// any website can be designed on the page.
 
-document.querySelector(".box").dataset
+document.querySelector(".box").dataset;
 // DOMStringMap {createdby: 'nahid'}, will show dataset
 
 // insertion method
 let div = document.createElement("div");
-div.innerHTML = "it is inserted by using <b>js</b>"
+div.innerHTML = "it is inserted by using <b>js</b>";
 div.setAttribute("class", "created");
 document.querySelector(".container").append(div);
 // the element will be added at the end for using append
@@ -74,19 +73,18 @@ document.querySelector(".container").before(div);
 // the element will be added before element for using before
 
 // insert adjacent html
-let cont = document.querySelector(".container")
+let cont = document.querySelector(".container");
 cont.insertAdjacentHTML("afterend", "i am inserted using adjacent");
 // it will add after the element
 
-document.querySelector(".container").classList
+document.querySelector(".container").classList;
 // DOMTokenList ['container', value: 'container']
 
-document.querySelector(".container").className
-//'container' 
+document.querySelector(".container").className;
+//'container'
 
-document.querySelector(".container").classList.add("nahid")
+document.querySelector(".container").classList.add("nahid");
 // class will be added, one class cant be added multiple times
 
-document.querySelector(".container").classList.toggle("nahid")
+document.querySelector(".container").classList.toggle("nahid");
 // it is like on off switch
-
