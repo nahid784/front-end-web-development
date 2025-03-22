634a5dbb07bb0ee0c62b72d1a24e30aaf5b04847
Nahid Hasan Chowdhury <112480805+nahid784@users.noreply.github.com>
2025-03-22 16:48:45 +0600

    Update callback.js

--- JS/ch13_callback&Promises/callback.js
+++ JS/ch13_callback&Promises/callback.js
@@ -8,24 +8,46 @@ setTimeout(() => {
     "this is third line, but comes last because of Assynchronus nature of js"
   );
 }, 0);
+// will run in last for assynchronus
 
 setTimeout(() => {
   console.log(
     "this is 4th line, but comes last because of Assynchronus nature of js"
   );
 }, 0);
+// will run in last for assynchronu s
 
 console.log("this is last line");
 
 // CallBack Function
-const callBack = (arg) => {
+// normal callback
+// const callBack = (arg) => {
+//   console.log(arg);
+// };
+// const loadScript = (src, callBack) => {
+//   let sc = document.createElement("script");
+//   sc.src = src;
+//   sc.onload = callBack("Nahid after loaded script");
+//   document.head.append(sc);
+// };
+
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callBack)
+
+// pyramid of doom, callback hell
+const fn = () =>{
+  console.log("Callback Hell");
+}
+
+const callBack = (arg, fn) =>{
   console.log(arg);
-};
-const loadScript = (src, callBack) => {
+  fn();
+}
+
+const loadScript = (src, callBack) =>{
   let sc = document.createElement("script");
   sc.src = src;
-  sc.onload = callBack("Nahid after loaded script");
+  sc.onload = callBack("this is pyramid of doom", fn);
   document.head.append(sc);
 };
 
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callBack)
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callBack);
