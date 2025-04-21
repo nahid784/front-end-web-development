3ae78e8dd962159d51419292671a6eb5d6adc482
nahid784 <112480805+nahid784@users.noreply.github.com>
2025-04-21 23:30:39 +0600

    Update 01_magical_Sorting_Hat.js

--- JS/exercise/challanges/01_magical_Sorting_Hat.js
+++ JS/exercise/challanges/01_magical_Sorting_Hat.js
@@ -1,11 +1,11 @@
 // The Magical Sorting Hat: Imagine you are creating a magical sorting hat for a wizard school. Implement a JavaScript function that takes an array of student names and assigns them to one of the four houses (Gryffindor (length less than 6), Hufflepuff(length less than 8), Ravenclaw(length less than 12), or Slytherin(length greater than or equal to 12)) based on the length of their names.
 
 // method 01
-let sName = ["Nahid", "Chowdhury", "Zakiul", "KaziAbirSadman"];
+let sName = ["Nahid", "Chowdhury", "Zakiul", "KaziAbirSadman"];     // taking students name as array
 
 for (let i = 0; i < sName.length; i++) {
-    const element = sName[i].length;
-    const arrName = sName[i];
+    const element = sName[i].length;    // storing the length of the array names
+    const arrName = sName[i];    // storing the array names
 
     if (element < 6) {
         console.log(`${arrName} goes to Gryffindor`);
@@ -15,5 +15,5 @@ for (let i = 0; i < sName.length; i++) {
         console.log(`${arrName} goes to Ravenclaw`);
     } else if (12 <= element) {
         console.log(`${arrName} goes to Slytherin`);
-    }
+    }       // printing the assigned array names with house names
 }
