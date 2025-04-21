ae0bfefe7a199be5471a03891fd0fc7f01659021
nahid784 <112480805+nahid784@users.noreply.github.com>
2025-04-21 23:33:01 +0600

    Update 02_The Double Trouble.js

--- JS/exercise/challanges/02_The Double Trouble.js
+++ JS/exercise/challanges/02_The Double Trouble.js
@@ -1,8 +1,8 @@
 // The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
 
-function double() {
-    let arr2 = [];
-    let arr = [2, 2, 3, 4, 4, 5, 6];
+function double() {     // creating a function
+    let arr = [2, 2, 3, 4, 4, 5, 6];    // creating array to store elements
+    let arr2 = [];     // creating an array to store new elements
     console.log(arr);
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === arr[i - 1]) {
@@ -10,7 +10,7 @@ function double() {
         }
         else{
             arr2.push(arr[i] * 2);
-        }
+        }        // checking every array value and checking if the array contains any duplicate elements or not
     }
     console.log(arr2);
 }
